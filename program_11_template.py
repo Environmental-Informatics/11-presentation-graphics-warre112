8605d5e3b6ff3cdadfd71abd7f9f0068a889bb7b
Keith Aric Cherkauer <cherkaue@purdue.edu>
2020-04-10 18:29:58 -0400

    Initial commit

---
+++ program_11_template.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+# Add your own header comments
+#
+
+def ReadMetrics( fileName ):
+    """This function takes a filename as input, and returns a dataframe with
+    the metrics from the assignment on descriptive statistics and 
+    environmental metrics.  Works for both annual and monthly metrics. 
+    Date column should be used as the index for the new dataframe.  Function 
+    returns the completed DataFrame."""
+    
+    return( DataDF )
+
+
+# the following condition checks whether we are running as a script, in which 
+# case run the test code, otherwise functions are being imported so do not.
+# put the main routines from your code after this conditional check.
+
+if __name__ == '__main__':
+
+    # define full river names as a dictionary so that abbreviations are not used in figures
+    riverName = { "Wildcat": "Wildcat Creek",
+                  "Tippe": "Tippecanoe River" }
+    
+ 
